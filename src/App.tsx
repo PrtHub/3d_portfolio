5e5764554e108cfde50ee5ff3f1729fc215ed555
Pritam <pri10145@gmail.com>
2024-10-06 22:22:43 +0530

    clients

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./sections/About"
+import Clients from "./sections/Clients"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
 import Projects from "./sections/Projects"
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Projects/>
+      {/* <Clients/> */}
     </main>
   )
 }
